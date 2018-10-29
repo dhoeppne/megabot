e20f043852828388e09a9eb7cda44e14a1dc5569
Hoeppner <david_hoeppner@intuit.com>
2018-10-28 22:51:14 -0600

    Bringing up to JS ES6 coding standards, and fixing some repeated code

---
+++ constants.js
@@ -0,0 +1,6 @@
+export default {
+    channelRoles: ["artists", "directors", "developers", "musicians", "producers", "writers"],
+    courseRoles: ["blaw-301", "cmput-250", "cmput-366", "mlcs-399"],
+    adminRole: "Daddy",
+    modRole: "Sugar Babies"
+}
